6d3af8ae013e7c1345b70ee29ffaad31ddc0c65a
FreekO <freek@olgers.com>
2021-09-07 22:24:25 +0200

    with working test

---
+++ src/SailBoatEngine.ts
@@ -0,0 +1,13 @@
+export class SailBoatEngine {
+    private _slratio = 1.34
+
+    
+
+   /*  public set slratio(slratio: number) {
+        this._slratio = slratio
+    }*/
+
+    public get slratio() {
+        return this._slratio
+    } 
+}
