0b9985196c15370c3ed9832dcf233afca348798d
FreekO <freek@olgers.com>
2021-09-07 22:37:34 +0200

    update

--- src/__tests__/getSettings.test.ts
+++
@@ -1,6 +0,0 @@
-import { SailBoatEngine } from "../SailBoatEngine";
-
-it("get value of sl ratio", () => {
-    const SBE = new SailBoatEngine()
-    expect(SBE.slratio).toEqual(1.34);
-});
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 export class SailBoatEngine {
     private _slratio = 1.34
 
-    
-
-   /*  public set slratio(slratio: number) {
+    public set slratio(slratio: number) {
         this._slratio = slratio
-    }*/
+    }
 
     public get slratio() {
         return this._slratio
-    } 
+    }
 }
