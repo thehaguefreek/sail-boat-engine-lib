c9686b53e0f1f6b01ec0da1b0f3b32651745932c
FreekO <freek@olgers.com>
2021-09-08 22:58:29 +0200

    set slratio back to default value

--- src/__tests__/values.test.ts
+++ src/__tests__/values.test.ts
@@ -1,12 +1,14 @@
 import { SailBoatEngine } from "..";
 
+let SBE = new SailBoatEngine()
+
 it("Check default value of slratio", () => {
-    const SBE = new SailBoatEngine()
     expect(SBE.slratio).toEqual(1.34);
 });
 
-it("Check setter for slratio", () => {
-    const SBE = new SailBoatEngine()
+it("Check set and get slratio", () => {
     SBE.slratio = 5.42
+    expect(SBE.slratio).toEqual(5.42);
+    SBE.slratio = 1.34
     expect(SBE.slratio).toEqual(5.42);
 });
